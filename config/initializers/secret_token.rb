40b3d6767518e2f966742ca951632249776755e2
Enrico Teotti <enrico.teotti@gmail.com>
2013-11-11 15:18:56 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RentalAgency::Application.config.secret_key_base = '6a6589eae0bc02c92fbdbd465827986e96796ebedad9d3fbfba332cabdd5d12b85b5d2ee308829a89e8ba8a881c7e07af69c76502ab05ba96d16d5e7d82e75fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RentalAgency::Application.config.session_store :cookie_store, key: '_rental_agency_session'
